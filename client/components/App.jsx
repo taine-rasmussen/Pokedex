8b1de0ab9d9a44bda2baed5589fe2e7e8e3f6935
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-09-17 16:09:24 +1200

    added basic html markup for landing page

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,10 +6,36 @@ function App () {
   return (
     <>
       <div className='app'>
-        <h1>Pokedex working</h1>
+
+        <div className='poke-header-container'>
+          <h1 className='poke-header'>Pokedex</h1>
+        </div>
+
+        <div className='poke-search-container'>
+          <input className='poke-search'></input>
+        </div>
+
+        <div className='poke-btn-container'>
+          <button className='poke-btn-search'>Search</button>
+          <button className='poke-btn-random'>Random</button>
+        </div>
       </div>
     </>
   )
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+// MVP
+// Set landing page with react components - no routing needed
+// default page large search space (eg google) that prompts the search for a pokemon - sends get request to api and fills page with information
+// next to search btn will be a random btn that will generate random pokemon on screen
+// Ability to clear or search again
