03a58a45317a252465faa7608ec9fdb5feac9cab
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-09-22 20:14:48 +1200

    set result img to top left of profile container

--- client/components/results.jsx
+++ client/components/results.jsx
@@ -6,9 +6,9 @@ const Results = ({data}) => {
         <>
             <div className="results-container">
                 <div className="results-profile-container">
-                    <div className="results-profile-img">
-                        {data.sprites ? (<img src={data.sprites.front_default} className='single-img' alt={data.name} />) : (null)}
-                    </div>
+
+                        {data.sprites ? (<img src={data.sprites.front_default} className='results-profile-img' alt={data.name} />) : (null)}
+
                     <div className="results-profile-info">
                         <h3>Name: {data.name}</h3> 
                         <h3>Weight: {data.weight}</h3>
