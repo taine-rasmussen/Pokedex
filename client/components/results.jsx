3463ff0366a5f7829a273a3dbeeae755e61b29c2
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-09-22 20:01:11 +1200

    centered content on reults page

--- client/components/results.jsx
+++ client/components/results.jsx
@@ -4,10 +4,13 @@ const Results = ({data}) => {
 
     return(
         <>
-            <h3>Name: {data.name}</h3> 
-            <h3>Weight: {data.weight}</h3>
-            <h3>Height: {data.height}</h3>
-            {data.sprites ? (<img src={data.sprites.front_default} className='single-img' alt={data.name} />) : (null)}
+            <div className="results-container">
+                <h3>Name: {data.name}</h3> 
+                <h3>Weight: {data.weight}</h3>
+                <h3>Height: {data.height}</h3>
+                {data.sprites ? (<img src={data.sprites.front_default} className='single-img' alt={data.name} />) : (null)}
+            </div>
+
         </>
     )
 }
