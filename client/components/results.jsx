f98748f5bee72b4971cf3cd4976e2dbb97999c99
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-09-24 14:27:12 +1200

    pokemonWeight set to string

--- client/components/results.jsx
+++ client/components/results.jsx
@@ -2,11 +2,17 @@ import React, { useState } from 'react';
 
 const Results = ({data}) => {
 
-    let pokemonWeight = data.weight
+    // Gets pokemon weight data and converts to string
+    let pokemonWeight = data.weight.toString()
+
     // Converts weight data for pokemon to kg
     const convertWeight = () => {
 
-        console.log('weight test:', pokemonWeight)
+        
+
+
+
+        console.log('weight test:', pokemonWeight.length);
         // weight comes through as 60 should be 6.0kg / 4600 should be 460.0kg
         // Func insterts decimal point to left of last 0 on data.weight
         // Pokemon without without weith ending in 0 364 should be 36.4kg
